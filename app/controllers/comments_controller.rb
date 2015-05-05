af3259b2f8312536b2fc7612c7cc469f6f448030
shibabu <shibabu.alfa@gmail.com>
2015-05-06 04:48:35 +0900

    Added comments and relationship on posts members

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,29 @@
 class CommentsController < ApplicationController
   def create
     @post=Post.find params[:post_id]
-    @comment=@post.comments.create comment_params
+    @comment=@post.comments.new comment_params
+    if @comment.save
+      redirect_to post_path(@post), notice: 'Comment created..'
+    else
+      render post_path(@post), flash.now[:alert]='Could not create comment. Check errors below:'
+    end
 
-    redirect_to post_path(@post)
   end
 
   def destroy
+    @post=Post.find params[:post_id]
+    @comment=@post.comments.find params[:id]
+
+    if @comment.destroy
+      redirect_to post_path(@post), notice: 'Comment deleted'
+    else
+      render post_path(@post), flash.now[:alert]='Could not delete comment. Check errors below:'
+    end
+  end
+
+
+  private
+  def comment_params
+    params.require(:comment).permit :name, :body
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, :body, presence: true, length: {minimum: 8}
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
